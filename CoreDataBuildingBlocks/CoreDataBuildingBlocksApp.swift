dafbc35f3a21d0766cfc0feac53655031a4bfe2f
Mohammad Azam <azamsharp@gmail.com>
2021-02-21 09:50:47 -0600

    Initial Commit

---
+++ CoreDataBuildingBlocks/CoreDataBuildingBlocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataBuildingBlocksApp.swift
+//  CoreDataBuildingBlocks
+//
+//  Created by Mohammad Azam on 2/21/21.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataBuildingBlocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
